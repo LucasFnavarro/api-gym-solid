aeb7c8a5b6e84302f7d28f48a4d86f044eac6a7b
Lucas Navarro <navarrolucas0009@gmail.com>
2025-10-02 12:08:54 -0300

    Implement ValidateCheckIn use case; add LateCheckInValidationError and corresponding tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -5,6 +5,16 @@ import { CheckInsRepository } from "../check-ins-repository";
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf("date"); // 00:00 (meia noite)
     const endOfTheDay = dayjs(date).endOf("date"); // 23:59 fim do dia
@@ -24,7 +34,7 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
-      id: crypto.randomUUID(),
+      id: data.id ?? crypto.randomUUID(),
       user_id: data.user_id,
       gym_id: data.gym_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
@@ -44,5 +54,15 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
   async countByUserId(userId: string) {
     return this.items.filter((item) => item.user_id === userId).length;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
+
+    return checkIn;
   }
 }
